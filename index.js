de1c7a0e8192a8b92d919895a5d6aa79885cf6fe
erikn775 <erikn775@gmail.com>
2021-02-11 13:54:11 -0700

    faces can be displayed now from fetch

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-fetch('http://localhost:3000/cards').then(resp => resp.json()).then(resp => console.log(resp))
+function fetchCards() {
+    fetch('http://localhost:3000/cards')
+        .then(resp => resp.json())
+        .then(cardData => addCardsToDom(cardData))
+        
+}
+const currentDiv = document.getElementById("div1");
+const cardList = document.querySelector("#card-list");
+
+function addCardsToDom(cardData){
+
+    if(cardData.error){
+        alert(cardData.error.message)
+    } 
+    else {
+        //debugger;
+    const faces = cardData.map(card => card.face)
+    for(let i = 0; i < faces.length; i++){
+        cardList.innerHTML += `<li>${faces[i]}</li>`;
+        }
+    }
+}
+
+function handleError(e){
+    console.log(e)
+}
+
+function addElement() {
+  const newDiv = document.createElement("div");
+  const newContent = document.createTextNode();
+  newDiv.appendChild(newContent);
+  
+  document.body.insertBefore(newDiv, currentDiv);
+}
+
+currentDiv.addEventListener('click', function(){
+    addElement()
+})
