9810a6bc8715db1e82a97741c035cb0f5ef77ac7
erikn775 <erikn775@gmail.com>
2021-02-12 12:19:09 -0700

    refactoring to make code oo

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-const cardUrl = 'http://localhost:3000/cards/'
-
-const searchButton = document.getElementById('submit')
-searchButton.addEventListener('click', function(){
-    fetchCards();
+window.addEventListener('DOMContentLoaded', function() {
+    Card.addCardDropDown();
 })
 
-function cardLists() {
-    const query = document.querySelector(`#themes`)
-    return query.value
-}
-function fetchCards() {
-    
-    fetch(cardUrl + cardLists())
-        .then(resp => resp.json())
-        .then(cardData => addCardsToDom(cardData))
-        
-}
 
-const cardList = document.querySelector("#card-list");
-
-function addCardsToDom(cardData){
-
-    const faces = cardData.faces
-    for(let i = 0; i < faces.length; i++){
-        cardList.innerHTML += `<li>${faces[i]}</li>`;
-    }
-}
-
-  
 
--- src/cards.js
+++ src/cards.js
@@ -1,3 +1,7 @@
+const cardUrl = 'http://localhost:3000/cards/'
+const cardList = document.querySelector("#card-list");
+const searchButton = document.getElementById('submit')
+
 class Card {
     static all = []
     constructor(id, theme, faces){
@@ -13,5 +17,33 @@ class Card {
         newOption.text = `${this.theme}`
     }
 
+    fetchCards() {
+        fetch(cardUrl + cardLists())
+            .then(resp => resp.json())
+            .then(cardData => addCardsToDom(cardData))
+            
+    }
+
+    addCardsToDom(cardData){
+        const faces = cardData.faces
+        for(let i = 0; i < faces.length; i++){
+            cardList.innerHTML += `<li>${faces[i]}</li>`;
+        }
+    }
+
 }
   
+
+
+searchButton.addEventListener('click', function(){
+    fetchCards();
+})
+
+function cardLists() {
+    const query = document.querySelector(`#themes`)
+    return query.value
+}
+
+
+
+
