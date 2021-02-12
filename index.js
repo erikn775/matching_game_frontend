c3d8101ceec3f46647d2765a04f452a08a5e7f08
erikn775 <erikn775@gmail.com>
2021-02-11 20:32:10 -0700

    fetch requests from backend now working

--- index.js
+++ index.js
@@ -1,38 +1,30 @@
+const cardUrl = 'http://localhost:3000/cards/'
+
+const searchButton = document.getElementById('submit')
+searchButton.addEventListener('click', function(){
+    fetchCards();
+})
+
+function cardLists() {
+    const query = document.querySelector(`#themes`)
+    return query.value
+}
 function fetchCards() {
-    fetch('http://localhost:3000/cards')
+    
+    fetch(cardUrl + cardLists())
         .then(resp => resp.json())
         .then(cardData => addCardsToDom(cardData))
         
 }
-const currentDiv = document.getElementById("div1");
+
 const cardList = document.querySelector("#card-list");
 
 function addCardsToDom(cardData){
 
-    if(cardData.error){
-        alert(cardData.error.message)
-    } 
-    else {
-        //debugger;
-    const faces = cardData.map(card => card.face)
+    const faces = cardData.faces
     for(let i = 0; i < faces.length; i++){
         cardList.innerHTML += `<li>${faces[i]}</li>`;
-        }
     }
 }
+    
 
-function handleError(e){
-    console.log(e)
-}
-
-function addElement() {
-  const newDiv = document.createElement("div");
-  const newContent = document.createTextNode();
-  newDiv.appendChild(newContent);
-  
-  document.body.insertBefore(newDiv, currentDiv);
-}
-
-currentDiv.addEventListener('click', function(){
-    addElement()
-})
