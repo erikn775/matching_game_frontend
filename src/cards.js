2595d2e868f0f04483b0ef4d448aa99d654002f7
erikn775 <erikn775@gmail.com>
2021-02-16 16:07:45 -0700

    added score feature

--- src/cards.js
+++ src/cards.js
@@ -23,6 +23,8 @@ const moveHeader = document.getElementById("end-move");
 const topTimer = document.getElementById("top-timer");
 const endTimer = document.getElementById("end-timer");
 let minute = 0, second = 0;
+const endScore = document.getElementById("end-score")
+let score = 100000
 
 function addCardDropDown() {
     fetch(cardUrl)
@@ -181,7 +183,8 @@ function moveTopCounter(){
 function moveEndCounter(){
     moveHeader.innerHTML = `Number of Moves: ${moveCount}`
     moveForm = moveCount
-
+    let newScore = score - (moveCount * 1923)
+    endScore.innerHTML = `Score: ${newScore}`
 }
 
 function timer(){
@@ -197,6 +200,9 @@ function timer(){
     }, 1000)
     
 }
+
+
+
 
 
 
